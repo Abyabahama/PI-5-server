8b0d2f08ca469b4a5c367bd89e5cad2509feb70e
unknown <22.10121-7@maua.br>
2024-12-03 15:53:51 -0300

    implementa cadastros de aplicadores

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/aplicador/pj/cadastro', (req, res) => {
   if (!RG || !nome || !CPF) {
     return res.status(400).json({ error: 'Os campos RG, Nome e CPF são obrigatórios.' });
   }
-  const sql = `INSERT INTO aplicador_PJ (RG, Nome, Nacionalidade, EstCivil, Profissao, CPF, Email, Endereco, Cidade, Telefone, UF, Curriculo, URL, Linkedin, Portfolio, Lattes, Valor, Empresa, CNPJ, CNAE, NomeResponsavel, EmailResponsavel, CPFResponsavel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+  const sql = `REPLACE INTO aplicador_PJ (RG, Nome, Nacionalidade, EstCivil, Profissao, CPF, Email, Endereco, Cidade, Telefone, UF, Curriculo, URL, Linkedin, Portfolio, Lattes, Valor, Empresa, CNPJ, CNAE, NomeResponsavel, EmailResponsavel, CPFResponsavel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
   // Array de valores para preencher os placeholders no comando SQL
   const values = [
     RG, nome, nacionalidade, estCivil, profissao, CPF, email,
@@ -39,7 +39,7 @@ app.post('/aplicador/pj/cadastro', (req, res) => {
   ];
   // Executando o SQL
   db.query(sql, values, (err, results) => {
-    if (err) {
+    if (err) { 
       console.error('Erro ao inserir dados:', err);
   
       return res.status(500).json({ error: 'Erro ao inserir dados.' });
@@ -47,9 +47,67 @@ app.post('/aplicador/pj/cadastro', (req, res) => {
 
     return res.status(201).json({ message: 'Dados inseridos com sucesso!' });
     
-  });
+  }); 
 });
 
+app.post('/aplicador/rpa/cadastro', (req, res) => {
+    // Extrai os dados do corpo da requisição 
+    const {
+      RG, nome, nacionalidade, estCivil, profissao, CPF, email,
+      endereco, cidade, telefone, UF, curriculo, URL, linkedin, 
+      portfolio, lattes
+    } = req.body; 
+    // Validação para garantir que os campos obrigatórios estejam preenchidos
+    if (!RG || !nome || !CPF) {
+      return res.status(400).json({ error: 'Os campos RG, Nome e CPF são obrigatórios.' });
+    }
+    const sql = `REPLACE INTO aplicador_rpa (RG, Nome, Nacionalidade, EstCivil, Profissao, CPF, Email, Endereco, Cidade, Telefone, UF, Curriculo, URL, Linkedin, Portfolio, Lattes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    // Array de valores para preencher os placeholders no comando SQL
+    const values = [
+      RG, nome, nacionalidade, estCivil, profissao, CPF, email,
+      endereco, cidade, telefone, UF, curriculo, URL, linkedin, 
+      portfolio, lattes
+    ];
+    // Executando o SQL
+    db.query(sql, values, (err, results) => {
+      if (err) { 
+        console.error('Erro ao inserir dados:', err);
+    
+        return res.status(500).json({ error: 'Erro ao inserir dados.' });
+      }
+  
+      return res.status(201).json({ message: 'Dados inseridos com sucesso!' });
+      
+    }); 
+})
+
+app.post('/aplicador/imt/cadastro', (req, res) => {
+  // Extrai os dados do corpo da requisição 
+  const {
+    RG, email
+  } = req.body; 
+  // Validação para garantir que os campos obrigatórios estejam preenchidos
+  if (!RG || !email) {
+    return res.status(400).json({ error: 'Os campos RG e E-mail são brigatorios' });
+  }
+  const sql = `REPLACE INTO aplicador_maua (RG, Email) VALUES (?, ?)`
+  // Array de valores para preencher os placeholders no comando SQL
+  const values = [
+    RG, email
+  ];
+  // Executando o SQL
+  db.query(sql, values, (err, results) => {
+    if (err) { 
+      console.error('Erro ao inserir dados:', err);
+  
+      return res.status(500).json({ error: 'Erro ao inserir dados.' });
+    }
+
+    return res.status(201).json({ message: 'Dados inseridos com sucesso!' });
+    
+  }); 
+})
+ 
 app.listen(4000, () => {
   console.log("Aplicadores. Porta 4000")
-}) 
+})  
